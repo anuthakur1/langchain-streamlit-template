a770adb3f38b421a8a173e16b250762a44d7762a
Anu Thakur <85850320+anuthakur1@users.noreply.github.com>
2023-07-30 22:14:23 -0700

    added api key

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain.llms import OpenAI
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
     llm = OpenAI(temperature=0)
     chain = ConversationChain(llm=llm)
     return chain
